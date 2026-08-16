347456b478298c7cd15a92941390869b7c85d951
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-08-16 15:56:14 -0300

    feat: adiciona endpoint para excluir equipamento

--- src/main/java/com/projects/chamados/controllers/EquipmentController.java
+++ src/main/java/com/projects/chamados/controllers/EquipmentController.java
@@ -3,12 +3,12 @@ package com.projects.chamados.controllers;
 import com.projects.chamados.dtos.outputs.EquipmentOutputDTO;
 import com.projects.chamados.services.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value="/equipments")
@@ -21,5 +21,12 @@ public class EquipmentController {
         var equipments = this.equipmentService.listAll();
 
         return ResponseEntity.ok().body(equipments);
+    }
+
+    @DeleteMapping(value="/{equipmentId}")
+    public ResponseEntity<Void> delete(@PathVariable UUID equipmentId){
+        this.equipmentService.deleteById(equipmentId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
